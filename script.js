f1d62dd52309190db23f681d032fb9761c9a31f2
irfanfal <falihmahdika@gmail.com>
2023-08-15 14:34:47 +0700

    update javascript

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 
-
-
-
 const startBTN = document.getElementById("start-button");
+const form = document.getElementById("my-form");
+const submitBTN = document.getElementById("submit")
+const gameboard = document.querySelector(".game-board")
+const control = document.querySelector(".controls")
 
-startBTN.addEventListener("click", () => {
-    alert("hello world");
+
+
+startBTN.addEventListener("click", (event) => {
+    event.preventDefault();
+    form.style.display = "grid";
+    control.style.display = "none";
+})
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+    form.style.display = "none";
+    gameboard.style.display = "grid";
+    console.log(data);
 })
